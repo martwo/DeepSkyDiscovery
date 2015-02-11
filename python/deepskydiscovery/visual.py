54ab120454ab0dad01d72fe0c677152f4a67df9e
Martin Wolf <martin.wolf@fysik.su.se>
2015-02-11 20:37:31 +0100

    Add colorbar to the image view.

--- python/deepskydiscovery/visual.py
+++ python/deepskydiscovery/visual.py
@@ -1,21 +1,24 @@
 import numpy as np
 import deepskydiscovery as dsd
-import matplotlib.pyplot as plt
 
-def plot_image(ax, img):
+def plot_image(ax, img, cmap=None):
     import matplotlib.pyplot as plt
     import matplotlib.cm as cm
 
-    ax.imshow(
+    if(cmap is None):
+        cmap = cm.gist_heat
+    img = ax.imshow(
           img.data.T
         , extent=(0, img.size_x, 0, img.size_y)
         , origin='lower'
         , interpolation='nearest'
-        , cmap=cm.gist_rainbow
+        , cmap=cmap
     )
-    #ax.figure.colorbar(ax)
+    ax.figure.colorbar(img, ax=ax)
 
 def show_image(img, title=""):
+    import matplotlib.pyplot as plt
+
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
     plot_image(ax, img)
